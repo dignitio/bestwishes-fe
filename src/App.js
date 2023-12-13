5f4054b626d1fe2f9fc56e32d3260c5efcd33637
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2023-12-13 18:12:11 +0100

    testing commit to my branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from "components/Button";
 function App() {
   return (
     <div className="flex bg-black h-screen justify-center items-center flex-nowrap">
-      <Button type="submit">Click me</Button>
+      <Button type="submit">Click me by sam</Button>
     </div>
   );
 }
