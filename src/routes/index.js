3f8d34a8032489cfa4383e68641189629b119dab
Temilayo Subair <42721419+Fersubair40@users.noreply.github.com>
2023-12-23 13:53:07 +0100

    fix: routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,17 @@
-import AuthLayout from "layout/AuthLayout";
+import AuthGuard from "guards/AuthGuard";
+import { Suspense, lazy } from "react";
 import { useRoutes } from "react-router-dom";
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const Loadable = (Component) => (props) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+
+  return (
+    <Suspense fallback={<>loading</>}>
+      <Component {...props} />
+    </Suspense>
+  );
+};
 
 export default function Router() {
   return useRoutes([
@@ -9,7 +21,26 @@ export default function Router() {
     },
     {
       path: "/dashboard",
-      children: [],
+      element: (
+        <AuthGuard>
+          <div> dashboard layout</div>
+        </AuthGuard>
+      ),
+      children: [
+        {
+          element: <div>hello</div>,
+
+          index: true,
+        },
+        {
+          element: <Tribute />,
+
+          path: "tribute",
+        },
+      ],
     },
   ]);
 }
+
+// const Dashboard = Loadable(lazy(() => import("/")));
+const Tribute = Loadable(lazy(() => import("../pages/Dashboard/Tribute")));
