d5bb02357430001dbdcf9d5895de8449adf30a5d
Oluwatomini Jinadu <jinadutomini02@gmail.com>
2024-06-08 14:42:40 +0100

    responsive view of share option

--- src/components/ShareComponent/index.js
+++ src/components/ShareComponent/index.js
@@ -1,4 +1,3 @@
-// ShareWishlistModal.js
 import { useState } from "react";
 import Modal from "components/Modal";
 import { motion } from "framer-motion";
@@ -15,29 +14,29 @@ function ShareWishlistModal({ open, onClose }) {
 
   return (
     <Modal width={500} open={open} onClose={onClose}>
-      <div className="flex flex-col">
-        <span className="text-lg">Share</span>
-        <div className="flex gap-4 mt-5 pr-6 pb-1 overflow-auto overflow-y-hidden cursor-pointer">
+      <div className="flex w-full flex-col">
+        <span className="text-lg px-12 md:px-0 lg:px-0">Share</span>
+        <div className="flex gap-4 mt-3 lg:mt-5 md:mt-4 md:px-0 px-12 lg:pr-6 pb-1 scroll-bar-hide overflow-auto lg:overflow-auto lg:overflow-y-hidden cursor-pointer">
           {shareComponent.map((share) => (
-            <div key={share.id} onClick={() => handleIconClick(share.link)}>
-              <motion.div
-                whileTap={{ scale: 0.8 }}
-                whileHover={{ scale: 0.95 }}
-                className="flex flex-col gap-2 justify-center items-center"
-              >
-                <share.icon className="cursor-pointer" />
-                <span>{share.name}</span>
-              </motion.div>
-            </div>
+            <motion.div
+              key={share.id}
+              onClick={() => handleIconClick(share.link)}
+              whileTap={{ scale: 0.8 }}
+              whileHover={{ scale: 0.95 }}
+              className="flex flex-col gap-2 justify-center items-center"
+            >
+              <share.icon className="cursor-pointer" />
+              <span>{share.name}</span>
+            </motion.div>
           ))}
         </div>
-        <div className="bg-[#F0F1F5] mt-6 rounded-full w-full py-5 px-4 relative flex justify-between items-center">
-          <p>{currentUrl || "copy this item here lorem ipsum"}</p>
+        <div className="bg-[#F0F1F5] mt-6 ml-9 md:ml-0 rounded-full w-96 md:w-full lg:w-full py-2.5 px-6 lg:py-5 lg:px-4 relative flex gap-1 justify-between items-center">
+          <p className=" line-clamp-1 w-full ">{currentUrl || "copy this item here lorem ipsum"}</p>
           <motion.button
             type="button"
             whileTap={{ scale: 0.8 }}
             whileHover={{ scale: 0.95 }}
-            className="absolute right-3 bg-primary py-2 px-6 rounded-full text-white"
+            className=" bg-primary py-2 px-4 lg:py-2 lg:px-6 rounded-full text-white"
             onClick={() => {
               navigator.clipboard.writeText(currentUrl);
               notify();
